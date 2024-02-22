50ce3a908ad8d0bd49fbdd065257af817b874071
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-02-22 21:22:02 +0600

    added daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
+
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
 
       <h1 className='text-red-200'>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <button className="btn btn-secondary">Secondary</button>
+
       </div>
 
     </>
