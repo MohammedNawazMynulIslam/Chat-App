5fb3c86f31610ca6be638ed3a5b72b78064089b4
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-02-24 14:34:52 +0600

    added message container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 
@@ -5,7 +6,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar/>
-      {/* <MessageContainer/> */}
+      <MessageContainer/>
       </div>
   )
 }
