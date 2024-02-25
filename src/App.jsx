b177116c6ecbce2d7d8309dd0274f940687af617
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-02-25 19:21:36 +0600

    added route using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
-
-
+import { Login } from './pages/Login/Login'
+import SignUp from './pages/Signup/Signup'
 
 function App() {
   return (
     <>
     <div className='p-4 h-screen items-center justify-center flex'>
-<Home/>
+    <Routes>
+      <Route  path="/" element={<Home/>} />
+      <Route  path="/login" element={<Login/>} />
+      <Route  path="/signup" element={<SignUp/>} />
+    </Routes>
     </div>
     </>
   )
